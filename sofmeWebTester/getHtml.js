1eb0903c8b7f631b183127c6301f3e2c67783674
すー <aaaiiiuuu35@gmail.com>
2019-07-24 23:41:57 +0900

    Update getHtml.js

--- sofmeWebTester/getHtml.js
+++ sofmeWebTester/getHtml.js
@@ -2,11 +2,12 @@ const listFiles = require('./file').listFiles;
 const Enumerable = require('linq');
 
 
-const fileList = listFiles('G:\\マイドライブ\\file\\GitHub\\SofmeWeb');
+const webPath = 'G:\\マイドライブ\\file\\GitHub\\SofmeWeb';
+const fileList = listFiles(webPath);
 
 const htmlList = Enumerable.from(fileList)
     .where(x => new RegExp('.*\.html').test(x))
-    .select(x => x.replace('G:\\マイドライブ\\file\\GitHub\\SofmeWeb', ''))
+    .select(x => x.replace(webPath, ''))
     .toArray();
 console.log(htmlList);
 
