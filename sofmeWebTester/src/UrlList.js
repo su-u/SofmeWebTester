ec6432c4efa3b250358c9c77bf6bd966923c495f
すー <aaaiiiuuu35@gmail.com>
2019-08-05 10:16:54 +0900

    Update: get url list method

--- sofmeWebTester/src/UrlList.js
+++ sofmeWebTester/src/UrlList.js
@@ -3,43 +3,43 @@ const URL     = require('url');
 const Enumerable = require('linq');
 
 const TARGET = 'http://softmedia.sakura.ne.jp/';
-const MAX_COUNT = 200;
 
-let targetList = [];
-targetList.push(TARGET);
 
-const getUrl =  async () => {
-    for (let i = 0; i < targetList.length; i++) {
-        if(i > MAX_COUNT)break;
+const getUrlList = async (target, MAX_Count = 200) => {
+    let targetList = [];
+    targetList.push(TARGET);
+    const getUrl = async () => {
+        for (let i = 0; i < targetList.length; i++) {
+            if (i > MAX_Count) break;
 
-        console.log(`fetch:${targetList[i]}`);
-        const list = await client.fetch(targetList[i])
-            .then((result) => {
+            console.log(`${i} :fetch:${targetList[i]}`);
+            const fetchData = await client.fetch(targetList[i]);
+
+            const UrlExtraction = (data) => {
                 let _list = [];
-                result.$('a').each(function (idx) {
-                    let href = result.$(this).attr('href');
+                data.$('a').each(function (idx) {
+                    let href = data.$(this).attr('href');
                     if (!href) return;
 
                     href = URL.resolve(targetList[i], href);
                     href = href.replace(/\#.+$/, "");
-                    if(!new RegExp('.+\.html').test(href)) return;
-                    if(!new RegExp(TARGET).test(href)) return;
+                    if (!new RegExp('.+\.html').test(href)) return;
+                    if (!new RegExp(TARGET).test(href)) return;
                     _list.push(href);
                 });
                 return _list;
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-            .finally(() => {
-                console.log('終了');
-            });
-        targetList.push(...list);
-        targetList = Enumerable.from(targetList).distinct().toArray();
-        console.log(targetList);
-        console.log(targetList.length)
-    }
+            };
+            const urlList = UrlExtraction(fetchData);
+            targetList.push(...urlList);
+            targetList = Enumerable.from(targetList).distinct().toArray();
+        }
+    };
+    await getUrl();
+    return targetList;
 };
+getUrlList('http://softmedia.sakura.ne.jp/', 5)
+    .then(value => {
+        console.log(value);
+});
 
 
-getUrl();
