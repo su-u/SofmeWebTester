062b04b489d4e03d95ae5bb3943f1ac72807b15f
すー <aaaiiiuuu35@gmail.com>
2019-08-05 20:44:09 +0900

    Update main.js

--- sofmeWebTester/src/main.js
+++ sofmeWebTester/src/main.js
@@ -1,14 +1,33 @@
 const getUrlList = require('./UrlList').getUrlList;
 const writer = require('./writeFile').writer;
+const fs = require('fs');
+const webdriver = require('selenium-webdriver');
+const screenShot = require('./screenShot').screenShot;
+const Enumerable = require('linq');
+
+const { promisify } = require('util');
+const { Builder, By, until } = webdriver;
+const capabilities = webdriver.Capabilities.chrome();
+capabilities.set('chromeOptions', {
+    args: [
+        '--headless',
+        '--no-sandbox',
+        '--disable-gpu',
+        `--window-size=1980,1200`
+    ]
+});
 
 const main = () => {
-    getUrlList('http://softmedia.sakura.ne.jp/', 200)
-        .then(value => {
-            console.log(value.insideUrl);
-            console.log(value.outsideUrl)
+    getUrlList('http://softmedia.sakura.ne.jp/', 5)
+        .then(result => {
+            console.log(result.insideUrl);
+            console.log(result.outsideUrl);
+
+            const urlList = Enumerable.from(result.insideUrl).distinct().toArray();
+            urlList.forEach((value, index, array) => {
+                screenShot(value);
+            });
         });
-
-
 };
 
 main();
